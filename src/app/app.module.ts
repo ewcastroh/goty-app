1893ad89b5c647385dd68bf2eef5782449027f78
Eimer Castro <ecastro@corelogic.com>
2023-10-04 20:16:19 -0500

    Implementing initial structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentsModule } from './components/components.module';
+
+import { HomeComponent } from './pages/home/home.component';
+import { GotyComponent } from './pages/goty/goty.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GotyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { HorizontalBarChartComponent } from './horizontal-bar-chart/horizontal-bar-chart.component';
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    HorizontalBarChartComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavbarComponent,
+    HorizontalBarChartComponent
+  ]
+})
+export class ComponentsModule { }
