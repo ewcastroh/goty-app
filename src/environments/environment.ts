b0b8d89c799cbd116465a8e3131995b177fa229a
Eimer Castro <ewcastroh@users.noreply.github.com>
2023-10-20 18:15:50 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: false,
-  url: 'http://localhost:5000/goty-chart/us-central1',
-  firebase: {
-    apiKey: 'AIzaSyDiHC-KbwyEtPnhrbDMCMVYAAyE-Hb-8T4',
-    authDomain: 'goty-chart.firebaseapp.com',
-    databaseURL: 'https://goty-chart-default-rtdb.firebaseio.com',
-    projectId: 'goty-chart',
-    storageBucket: 'goty-chart.appspot.com',
-    messagingSenderId: '340377128247',
-    appId: '1:340377128247:web:86c517a1d5ba7fccd5deb2'
-  }
+  url: 'http://localhost:5000/goty-chart/us-central1'
 };
