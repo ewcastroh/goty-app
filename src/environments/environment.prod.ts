9f874565f5d3872c87e51fb4624af3257637bfc7
Eimer Castro <ewcastroh10@gmail.com>
2023-10-20 18:32:26 -0500

    Adding firebase configuration in env files.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  url: 'https://us-central1-goty-chart.cloudfunctions.net'
+  url: 'https://us-central1-goty-chart.cloudfunctions.net',
+  firebase: {}
 };
